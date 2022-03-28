26b04597c75a25ad42586abab46b77de91ef9577
Milena Karolewska <milena.karolewska@gmail.com>
2022-03-28 10:38:02 +0200

    created empty fragments and activities

---
+++ app/src/main/java/com/example/cattagram/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cattagram.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cattagram.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/cattagram/mainpage/MainPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cattagram.mainpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cattagram.R
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
