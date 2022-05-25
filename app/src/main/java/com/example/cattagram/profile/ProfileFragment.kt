f6340241db5614641d8d9aa35eb1ff31df62f889
Milena <milena.karolewska@gmail.com>
2022-05-25 11:32:24 +0200

    git added profile activity and fragment

--- app/src/main/java/com/example/cattagram/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/cattagram/profile/ProfileFragment.kt
@@ -1,25 +1,24 @@
-package com.example.cattagram.mainpage
+package com.example.cattagram.profile
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cattagram.R
 import com.example.cattagram.databinding.FragmentMainPageBinding
-import com.example.cattagram.login.LoginActivity
-import com.example.cattagram.profile.ProfileActivity
+import com.example.cattagram.databinding.FragmentProfileBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class MainPageFragment : Fragment() {
+
+class ProfileFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private var _binding: FragmentMainPageBinding? = null
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +32,8 @@ class MainPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMainPageBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-        _binding!!.menuBottom.ivProfile.setOnClickListener {
-            startActivity(Intent(activity, ProfileActivity::class.java))
-        }
 
         return binding.root
     }
@@ -45,7 +41,7 @@ class MainPageFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainPageFragment().apply {
+            ProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
