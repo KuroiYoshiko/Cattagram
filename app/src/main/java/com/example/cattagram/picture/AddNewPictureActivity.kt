e5639c3a68250076776d8128d35e696d5dfbf999
Milena <milena.karolewska@gmail.com>
2022-05-27 10:56:27 +0200

    fixed activities

--- app/src/main/java/com/example/cattagram/picture/AddNewPictureActivity.kt
+++ app/src/main/java/com/example/cattagram/picture/AddNewPictureActivity.kt
@@ -2,6 +2,7 @@ package com.example.cattagram.picture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cattagram.R
 
 class AddNewPictureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/cattagram/profile/EditProfileActivity.kt
+++ app/src/main/java/com/example/cattagram/profile/EditProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.cattagram.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cattagram.R
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
